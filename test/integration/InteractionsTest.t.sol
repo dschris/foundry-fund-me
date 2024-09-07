a32bb9b901611cd60a0530cecf20384331757a39
Chris Harrsch <dschrisprime@gmail.com>
2024-09-06 22:56:15 -0400

    Get all tests passing & update README

--- test/integration/InteractionsTest.t.sol
+++ test/integration/InteractionsTest.t.sol
@@ -5,18 +5,18 @@ pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../../src/FundMe.sol";
 import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
-import {FundFundMe} from "../../script/Interactions.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 
 contract InteractionsTest is Test {
-    function setUp() external {
-        FundMe fundMe;
-        address USER = makeAddr("user");
-        uint256 constant STARTING_BALANCE = 5 ether;
-        uint256 constant SEND_VALUE = 0.1 ether;
-        uint256 constant GAS_PRICE = 1;
+    FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant STARTING_BALANCE = 5 ether;
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant GAS_PRICE = 1;
 
+    function setUp() external {
         DeployFundMe deploy = new DeployFundMe();
-        fundMe = deployFundMe.run();
+        fundMe = deploy.run();
         vm.deal(USER, STARTING_BALANCE);
     }
 
